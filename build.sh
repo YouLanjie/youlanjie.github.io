6c5919ce38e576370d13e6ace3545bc04e2fabbf
QiufuLan <66255345+YouLanjie@users.noreply.github.com>
2022-07-02 07:23:16 +0800

    选择主题
    Set theme jekyll-theme-cayman
    
    Set theme jekyll-theme-cayman
    
    初次修改
    
    Set theme jekyll-theme-hacker
    
    移除文件
    
    添加文件
    
    Set theme jekyll-theme-hacker
    
    使用主题
    
    修改README.md文件
    
    移除不需要的文件
    
    修改文件
    
    更改图片
    
    纠正错误
    
    修改网页模板
    
    尝试纠正错误
    
    ...
    
    修改背景
    
    添加文件
    
    修复图片的显示问题
    
    再次修改
    
    ...
    
    添加图片
    
    纠正错误
    
    添加文件
    
    添加图片文件
    
    修改文章，增加图片、视频。纠正个别错别字
    
    修改文章，修复bug
    
    添加一篇Blog
    
    小修改
    
    修复增删一些错误
    
    全面更换使用的主题，撤回一篇博客
    
    增加文件头，修复与主题冲突的标题，依语法检查修正错误
    
    修改图片网址，添加修正后的博客
    
    修正一些小问题，补充内容
    
    上传图片，改变文件位置，修改文件的图片链接
    
    换用hexo搭建博客
    
    emmm
    
    ...
    
    aaa
    
    大变更
    
    hexo构建blog的最后一个提交
    
    改用hugo构建Blog
    
    更改主题
    
    更新
    
    更新
    
    将原本的语文笔记改为org文档
    
    完成笔记内容
    
    更新
    
    补充语文笔记，增加刀剑神域
    
    更新
    
    日常更新
    
    小修改
    
    开始手动更新Blog
    
    添加404页面
    
    增加图标，新增道法早材
    
    小说添加TXT文件，博客增加代码高亮
    
    将刀剑第一部分拆分出来，合集的译者注释改用脚注形式展现
    
    更改首页
    
    增加返回主页链接，修改大部分文章的主题
    
    小修改
    
    添加新文章
    
    更新主题
    
    修改网站的结构，添加新的构建脚本
    
    完善自动脚本
    
    增加描述
    
    合并两个git仓库
    
    正常修改内容
    
    更新一遍时间
    
    切换部分文章主题，更新主题，更新Emacs教程
    
    日常更新
    
    增加Unciv推荐
    
    更改主题，添加一篇不完善的文章
    
    更新Unciv教程
    
    更改使用的主题样式，重新编写首页、关于页面，更新脚本
    
    添加新文章，增加道法笔记内容
    
    添加一些笔记
    
    修改主题样式，适配窄宽显示器，更改css文件编码
    
    将脚注和文章信息移动到右侧
    
    增加文章目录展开动画
    
    移除动画以修正无法显示全部目录的问题
    
    增加拆成分集的刀剑神域文章
    
    增加Manim学习笔记
    
    微调样式，修复表格越界问题
    
    将语文笔记2中的思维导图打成文字稿，manim教程补充pip问题链接，主页一个链接修正
    
    更新manim笔记，刷新html文件
    
    Changed the note, added the pdf file
    
    更新两篇Blog，修正原笔记下划线变角标问题，增加设置
    
    添加一个setup文件
    
    Update the file
    
    将一些东西添加到列表
    
    Update 2024.10.1
    
    add something new
    
    Fix the problem of the banner
    
    更新文章
    
    增加泰拉游戏笔记
    
    更新manim笔记，增加一张图片，为Terraria笔记替换高清图片
    
    补充原有内容
    
    补充一点东西
    
    修改补充部分内容，为泰拉笔记补充了一些图片
    
    增加草剑合成单
    
    更新泰拉笔记
    
    修改完善部分细节
    
    压缩

---
+++ build.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/zsh
+
+#================================================================
+#   Copyright (C) 2023 YouLanjie
+#
+#   文件名称：build.sh
+#   创 建 者：youlanjie
+#   创建日期：2023年04月02日
+#   描    述：构建博客
+#
+#================================================================
+
+app_name="${0##*/}"
+
+# Show an INFO message
+# $1: message string
+_msg_info() {
+    local _msg="${1}"
+    [[ "${quiet}" == "y" ]] || printf '[%s] INFO: %s\n' "${app_name}" "${_msg}"
+}
+
+# Show a WARNING message
+# $1: message string
+_msg_warning() {
+    local _msg="${1}"
+    printf '[%s] WARNING: %s\n' "${app_name}" "${_msg}" >&2
+}
+
+# Show an ERROR message then exit with status
+# $1: message string
+# $2: exit code number (with 0 does not exit)
+_msg_error() {
+    local _msg="${1}"
+    local _error=${2}
+    printf '[%s] ERROR: %s\n' "${app_name}" "${_msg}" >&2
+    if (( _error > 0 )); then
+        exit "${_error}"
+    fi
+}
+
+usage() {
+	usagetext="\
+usage: build.sh [options]
+  options:
+     -m 创建博客到public目录下（不建议使用）
+     -b 构建博客列表与首页
+     -u 导出未更新的org为html页面
+     -U 强制更新所有的html页面
+     -n 不更新文件内容
+     -h 帮助信息"
+	echo $usagetext
+	exit $1
+}
+
+mk_public() {
+	_msg_info "检测是否存在public文件夹..."
+	if [[ ! -d public ]] {
+		if [[ -f public ]] {
+			_msg_error "文件\`public\`已存在且非文件夹" 1
+		}
+		_msg_warning "文件夹\`public\`不存在"
+		_msg_info "将创建public文件夹"
+		if [[ ! -d public ]] {
+			_msg_error "无法创建" 1
+		}
+	} else {
+		_msg_info "结果为真，将进行构建操作"
+	}
+	_msg_info "清除public目录"
+	find public |grep -v "\.git"|sed -n "s/public\//.\/public\//p"|sort -r|sed "s/^/rm -d '/"|sed "s/$/'/"|zsh
+	_msg_info "创建目录树"
+	find post -type d|sed -n "s/post\//.\/public\/post\//p"|sed "s/^/mkdir -p '/"|sed "s/$/'/"|zsh
+	_msg_info "复制文件"
+	find post -type f|grep -v "\.org"|sed "s/^/cp -r '/"|sed "s/ 'post\/\(.*\)$/ 'post\/\1' 'public\/post\/\1'/"|zsh
+	_msg_info "复制首页"
+	cp index.html public/
+	_msg_info "复制404页"
+	cp 404.html public/
+	_msg_info "复制主题与图片"
+	cp -r theme/ public/
+	cp -r img/ public/
+	_msg_info "Done!"
+}
+
+update_file() {
+	emacs -Q -nw ./src/fastsetup.el "$1" --eval "(eval-buffer \"fastsetup.el\")"
+}
+
+check_time() {
+	if [[ (! -f $2) || ($1 -nt $2) ]] {
+		_msg_info "Export '$1' ..."
+		if [[ $flag_no_exp == "false" ]] {
+			update_file $1
+		}
+		if [[ $flag_no_exp == "true" ]] {
+			echo "" >> $2
+		}
+		return 0
+	}
+	return -1
+}
+
+update() {
+	#update_file index.org
+	#echo $file_list
+	for name (post/**/*.org) {
+		out=$(echo $name|sed "s/\\.org$/.html/")
+		check_time "$name" "$out"
+	}
+}
+
+build() {
+	_msg_info "清空源文件"
+	echo "#+TITLE: TimeLine\n#+setupfile: ../setup.setup" > src/post2.org
+	_msg_info "构建列表中"
+	_msg_info "获取文件头并处理中"
+	file_list=(post/**/*.org)
+	line_max=$(echo $file_list|wc -l)
+	for name ($file_list) {
+		title=$(head -n 5 $name |grep -i '#+title: ')
+		declare -l title="$title"
+		title=$(echo $title|sed 's/^#+title:[ ]*//')
+
+		date=$(head -n 5 $name |grep -i '#+date: ')
+		declare -l date="$date"
+		date=$(echo $date|sed 's/^#+date:[ ]*//;s/<\(.*\)>/\1/;s/\([^ ]*\) [一|二|三|四|五|六|日] /\1 /')
+		if [[ $date == "" ]] {
+			continue
+		}
+
+		desc=$(head -n 5 $name |grep -i '#+description: ')
+		name=$(echo $name|sed 's/^/..\//')
+		if [[ $desc != "" ]] {
+			declare -l desc="$desc"
+			desc=$(echo $desc|sed 's/^#+description:[ ]*//')
+			printf '- /%s/ [[%s][%s]]\\\\\\\\\\n  %s\n' "$date" "$name" "$title" "$desc" >> src/post2.org
+		} else {
+			printf "- /%s/ [[%s][%s]]\n" "$date" "$name" "$title" >> src/post2.org
+		}
+	}
+	list=$(cat src/post2.org|sort -r)
+	echo $list > src/post2.org
+	_msg_info "列表输出完成"
+	_msg_info "导出子页面中..."
+	tmp="true"
+	check_time "404.org" "404.html"
+	check_time "about.org" "about.html"
+	check_time "src/post.org" "src/post.html" || tmp="false"
+	update_file src/post2.org
+	if [[ $tmp == "true" ]] {
+		_msg_info "导出首页中..."
+		# emacs index.org -nw --eval "(progn (org-html-export-to-html) (kill-emacs))"
+		update_file index.org
+	}
+	_msg_info "Done!"
+}
+
+flag_no_exp="false"
+
+while {getopts 'mbnuh?' arg} {
+	case $arg {
+		m) mk_public ;exit 0 ;;
+		b) build ;exit 0 ;;
+		u) update ;exit 0 ;;
+		n) flag_no_exp="true" ;;
+		h|?) usage 0;;
+		*) usage 1;;
+	}
+}
+
+if [[ $@ == "" ]] {
+	_msg_error "No options!"
+	usage 1
+} else {
+	_msg_error "Bad options!"
+	usage 1
+}
+
