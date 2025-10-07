1ad2456a53d9a414a7e93cac7f295733bd636eb7
YouLanjie <ylj18926385312@163.com>
2025-10-07 20:47:33 +0800

    修正文件标题

--- post/Novel/无职/process2.py
+++ post/Novel/无职/process2.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 def main():
-    f = Path("./无职转生～到了异世界就拿出真本事～_org-style.txt")
+    f = Path("./无职转生～到了异世界就拿出真本事～.org")
     con = f.read_text().splitlines()
     f = None
     t = ""
@@ -10,12 +10,17 @@ def main():
     chapter = ""
     for i in con:
         if i.startswith("* "):
-            print(i[2:])
             if f:
                 f.write_text(t)
             name = i[2:]
-            if " ".join(name.split()[:2]) != chapter:
-                chapter = " ".join(name.split()[:2])
+            num = 2
+            nsp = name.split()
+            print(nsp)
+            if nsp[2][-1] == "篇":
+                num = 3
+            if " ".join(nsp[:num]) != chapter:
+                chapter = " ".join(nsp[:num])
+                print(chapter)
                 f = Path(f"{count:03d}_{chapter}.org")
                 count+=1
                 t = f"#+title: {chapter}\n#+setupfile: ./setup.setup\n"
